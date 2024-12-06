2e212a0c45b19985de9bbe67fd4d699d99432da6
Matthew Stockinger <matt@matthewstockinger.com>
2024-12-06 08:16:15 -0600

    start tasks component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- bookmark 2:12 -->
+
+<script lang="ts">
+	import TasksForm from './tasks-form.svelte';
+	let message = 'Tasks App';
+</script>
+
+<main>
+	<h1>{message}</h1>
+	<TasksForm />
+</main>
+
+<style>
+	main {
+		margin: 1rem auto;
+		max-width: 800px;
+	}
+</style>
---
+++ src/routes/tasks-form.svelte
@@ -0,0 +1,16 @@
+<form>
+	<label>
+		Task
+		<input name="newTask" />
+	</label>
+	<div class="button-container">
+		<button>Add</button>
+	</div>
+</form>
+
+<style>
+	.button-container {
+		display: flex;
+		justify-content: end;
+	}
+</style>
