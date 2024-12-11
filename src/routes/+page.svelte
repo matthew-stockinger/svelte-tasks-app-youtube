0f89ad53766166b17ec9dd8c43a7818385fde888
Matthew Stockinger <matt@matthewstockinger.com>
2024-12-11 16:11:29 -0600

    add filter buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,28 @@
-<!-- bookmark 13:00 -->
+<!-- bookmark 15:43 -->
+ <!-- need to rewrite filteredTasks to $derived.by -->
 
 <script lang="ts">
 	import TasksForm from './tasks-form.svelte';
 	import TasksList from './tasks-list.svelte';
-	import type { Task } from './types';
+	import type { Filter, Task } from './types';
 
 	let message = 'Tasks App';
+	let currentFilter = $state<Filter>('all');
 	let tasks = $state<Task[]>([]);
 	let totalDone = $derived(tasks.filter((task) => task.done).length);
+	let filteredTasks = $derived(
+		tasks.filter((task) => {
+			if (currentFilter === 'all') {
+				return true;
+			} else if (currentFilter === 'done') {
+				return task.done;
+			} else {
+				return !task.done;
+			}
+		})
+	);
 
-	$inspect(tasks);
+	$inspect(currentFilter);
 
 	function addTask(newTask: string) {
 		tasks.push({
@@ -31,13 +44,38 @@
 <main>
 	<h1>{message}</h1>
 	<TasksForm {addTask} />
-	<p>{totalDone} / {tasks.length} tasks completed</p>
-	<TasksList {tasks} {toggleDone} {removeTask} />
+	<p>
+		{#if tasks.length}
+			{totalDone} / {tasks.length} tasks completed
+		{:else}
+			Add a task to get started
+		{/if}
+	</p>
+	{#if tasks.length}
+		<section class="button-container">
+			<button class="secondary" onclick={() => (currentFilter = 'all')}
+				>All</button
+			>
+			<button class="secondary" onclick={() => (currentFilter = 'todo')}
+				>Todo</button
+			>
+			<button class="secondary" onclick={() => (currentFilter = 'done')}
+				>Done</button
+			>
+		</section>
+	{/if}
+	<TasksList tasks={filteredTasks} {toggleDone} {removeTask} />
 </main>
 
 <style>
 	main {
 		margin: 1rem auto;
 		max-width: 800px;
+	}
+
+	.button-container {
+		display: flex;
+		justify-content: end;
+		gap: 0.5rem;
 	}
 </style>
